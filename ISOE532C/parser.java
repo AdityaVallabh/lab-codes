107bafd22d9d681c17b29da4a9dd062d73364197
AdityaVallabh <adityavallabhganti@gmail.com>
2018-09-05 17:46:15 +0530

    Add Cyclomatic complexity counter

--- ISOE532C/parser.java
+++ ISOE532C/parser.java
@@ -4,9 +4,22 @@
  * and open the template in the editor.
  */
 
+import java.awt.List;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -56,7 +69,7 @@ public class parser {
         while(m.find()) {
             // System.out.println(code.substring(m.start(), m.end()));
             declaration = code.substring(m.start(), m.end());
-            System.out.println(declaration);
+            // System.out.println(declaration);
             count += 1;
             for(int i = 0; i < declaration.length(); i++) {
                 if(declaration.charAt(i) == ',') {
@@ -69,9 +82,9 @@ public class parser {
     
     public static int getInitialized(String code, String dataType) {
         String regex = "[\\s+|;]"+dataType+"\\s+([\\**_]?[a-zA-Z0-9\\[\\]]*(\\s*=\\s*[a-zA-Z0-9_\"]*)?)((\\s*,\\s*)([\\**_]?[a-zA-Z0-9\\[\\]]*(\\s*=\\s*[a-zA-Z0-9_\"]*)?))*;";        String declaration;
-        
+        regex = "[\\s|;]"+ dataType +"\\s.*(,.*)*;"; // simpler regex, but still not perfect
         int initialized;
-        System.out.println(regex);
+        // System.out.println(regex);
         Pattern pattern = Pattern.compile(regex);
         Matcher m = pattern.matcher(code);
         initialized = 0;
@@ -91,16 +104,16 @@ public class parser {
     public static int getForLoops(String code) {
         String regex = "for\\s*\\(.*;.*;.*\\)";
         int count = 0;
-        System.out.println(regex);
+        // System.out.println(regex);
         Pattern pattern = Pattern.compile(regex);
         Matcher m = pattern.matcher(code);
         while(m.find()) {
-            System.out.println(code.substring(m.start(), m.end()));
+            // System.out.println(code.substring(m.start(), m.end()));
             count += 1;
         }
         return count;
     }
-    
+ 
     public static int getWhileLoops(String code) {
         String regex = "while\\s*\\(.*\\)";
         int count = 0;
@@ -108,7 +121,7 @@ public class parser {
         Pattern pattern = Pattern.compile(regex);
         Matcher m = pattern.matcher(code);
         while(m.find()) {
-            System.out.println(code.substring(m.start(), m.end()));
+            //System.out.println(code.substring(m.start(), m.end()));
             count += 1;
         }
         return count;
@@ -125,13 +138,13 @@ public class parser {
         Pattern pattern = Pattern.compile(regex);
         Matcher m = pattern.matcher(code);
         while(m.find()) {
-            System.out.println(code.substring(m.start(), m.end()));
+            // System.out.println(code.substring(m.start(), m.end()));
             count += 1;
         }
         return count;
     }
     
-    public static String getFunctions(String code) {
+    public static String getFunctionsCount(String code) {
         String regex = "((int)|(void)|(float)|(char)|(double))\\s+\\w+\\(.*\\)";
         int count = 0;
         String out = "";
@@ -146,6 +159,35 @@ public class parser {
         return out;
     }
     
+    public static ArrayList getFunctions(String code) {
+        ArrayList<String> functions = new ArrayList<>();
+        String regex = "((int)|(void)|(float)|(char)|(double))\\s+\\w+\\(.*\\)\\s*\\{";
+        int c = 0;
+        String out = "";
+        // System.out.println(regex);
+        Pattern pattern = Pattern.compile(regex);
+        Matcher m = pattern.matcher(code);
+        while(m.find()){
+            String fBegin = code.substring(m.end());
+            int bCount = 1;
+            String function = code.substring(m.start(), m.end());
+            for(int i = 0; i < fBegin.length(); i++) {
+                if(fBegin.charAt(i) == '{') {
+                    bCount += 1;
+                } else if(fBegin.charAt(i) == '}') {
+                    bCount -= 1;
+                }
+                function += fBegin.charAt(i);
+                if(bCount == 0) {
+                    functions.add(function);
+                    break;
+                }
+            }
+            c += 1;
+        }
+        return functions;
+    }
+    
     
     /**
      * @param args the command line arguments
@@ -153,8 +195,8 @@ public class parser {
      */
     public static void main(String args[]) throws IOException {
 
-        String code = getCode("/home/dedsec/Desktop/input.c");
-        FileOutputStream out = new FileOutputStream("/home/dedsec/Desktop/ouput.txt");
+        String code = getCode("/home/iiita/Desktop/dedsec/soe/input.c");
+        FileOutputStream out = new FileOutputStream("/home/iiita/Desktop/dedsec/soe/ouput.txt");
         // System.out.println(code);
         
         int n, intCount, floatCount, doubleCount, charCount, forLoops, whileLoops, conditionals;
@@ -167,9 +209,19 @@ public class parser {
         whileLoops = getWhileLoops(code);
         conditionals = getConditionals(code);
         
-        String fn = getFunctions(code);        
-        String output = "";
+        String fn = getFunctionsCount(code);
+        ArrayList<String> functions = getFunctions(code);
+        int tc = 0;
         
+        for(String func: functions){
+            String name = func.substring(func.indexOf(" "),func.indexOf("("));
+            int cnt = getConditionals(func) + getForLoops(func) + getWhileLoops(func);
+            tc += cnt+1;
+            System.out.println("Function " + name + "(): " + cnt + " + " + "1 = " + (cnt+1));
+        }
+        System.out.println("TC = " + tc);
+        
+        String output = "";        
         output += ("DataTypes: " + n) + "\n";
         output += ("intCount: " + intCount + " " + getInitialized(code, "int")) + "\n";
         output += ("floatCount: " + floatCount + " " + getInitialized(code, "float")) + "\n";
@@ -179,6 +231,7 @@ public class parser {
         output += ("whileLoops: " + whileLoops) + "\n";
         output += ("conditionals: " + conditionals) + "\n";
         output += ("\nFunctions:\n" + fn);
+        output += ("\nCyclomatic Complexity:\n" + tc);
         
         for(int i = 0; i < output.length(); i++) {
             out.write(output.charAt(i));
